17e9a494ee362a8d9d48778a1769fdb0399294d4
iyuu.cn <367013672@qq.com>
2020-02-25 21:03:39 +0800

    升级docker制作脚本，每次制作都是最新版。

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-curl -o ./IYUUAutoReseed.zip http://api.iyuu.cn/IYUUAutoReseed.zip
+wget -c https://gitee.com/ledc/IYUUAutoReseed/repository/archive/master.zip -O IYUUAutoReseed.zip
+wget -c http://api.iyuu.cn/uploads/vendor.zip -O vendor.zip
 unzip -o ./IYUUAutoReseed.zip -d /root
+unzip -o ./vendor.zip -d /root/IYUUAutoReseed
 rm ./IYUUAutoReseed.zip
+rm ./vendor.zip
 docker build -t iyuu:latest .
 docker run -it -v /root/IYUUAutoReseed:/var/www -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
--- docker/task.sh
+++ docker/task.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/root/bin
-docker exec -it IYUUAutoReseed php iyuu.php
+docker exec IYUUAutoReseed php iyuu.php
