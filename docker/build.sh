59b2d5b4ca89ee489049b4542676a033ff7df756
iyuu.cn <367013672@qq.com>
2020-03-13 23:29:51 +0800

    简化docker编译文件

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 wget -c https://gitee.com/ledc/IYUUAutoReseed/repository/archive/master.zip -O IYUUAutoReseed.zip
-wget -c http://api.iyuu.cn/uploads/vendor.zip -O vendor.zip
 unzip -o ./IYUUAutoReseed.zip -d /root
-unzip -o ./vendor.zip -d /root/IYUUAutoReseed
 rm ./IYUUAutoReseed.zip
-rm ./vendor.zip
 docker build -t iyuu:latest .
 docker run -it -v /root/IYUUAutoReseed:/var/www -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
