5880f563e8b059ed55da07b216b3e584328346ff
iyuu.cn <367013672@qq.com>
2020-03-23 13:04:32 +0800

    增加通用备份脚本，可以添加进计划任务。

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# 传入的种子备份参数
+if [ $1 ]; then
+	AppName=$1
+fi
+
+if [ $2 ]; then
+	torrentDir=$2
+fi
+
+# 脚本当前目录
+pwddir=$(cd $(dirname $0); pwd)
+# 当前日期
+DATE=$(date +%Y%m%d)
+# 备份在当前目录
+backupdir=$pwddir"/"$AppName$DATE
+echo "种子备份目录："$backupdir
+mkdir $backupdir -p
+# 种子目录
+torrentDir=$torrentDir"/*"
+# 备份
+cp -rf $torrentDir $backupdir
+# 成功提示
+echo "ok";
