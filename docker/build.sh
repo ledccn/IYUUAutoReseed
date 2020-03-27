c4a8cef4ce371fe6d288371dd519ea332834fd32
iyuu.cn <367013672@qq.com>
2020-03-27 14:58:50 +0800

    修改build.sh脚本，新增小钢炮专用脚本

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-wget -c https://gitee.com/ledc/IYUUAutoReseed/repository/archive/master.zip -O IYUUAutoReseed.zip
-unzip -o ./IYUUAutoReseed.zip -d /root
-rm ./IYUUAutoReseed.zip
+#wget -c https://gitee.com/ledc/IYUUAutoReseed/repository/archive/master.zip -O IYUUAutoReseed.zip
+#unzip -o ./IYUUAutoReseed.zip -d /root
+#rm ./IYUUAutoReseed.zip
 docker build -t iyuu:latest .
 docker run -it -v /root/IYUUAutoReseed:/var/www -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
--- docker/n1.sh
+++ docker/n1.sh
@@ -5,3 +5,4 @@ cd /root/IYUUAutoReseed/docker
 chmod +x ./*.sh
 docker build -t iyuu:latest .
 docker run -it -v /root/IYUUAutoReseed:/var/www -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
+./iyuu.sh
