68e128e5cd64f0eecdf1e2c3de7af66850ff9ef6
iyuu.cn <367013672@qq.com>
2020-04-26 22:13:22 +0800

    小钢炮安装脚本添加/BT_backup目录挂载至docker

--- docker/n1.sh
+++ docker/n1.sh
@@ -4,5 +4,5 @@ git clone https://gitee.com/ledc/IYUUAutoReseed.git
 cd /root/IYUUAutoReseed/docker
 chmod +x ./*.sh
 docker build -t iyuu:latest .
-docker run -it -v /root/IYUUAutoReseed:/var/www -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
+docker run -it -v /root/IYUUAutoReseed:/var/www -v /var/lib/qbittorrent/.local/share/data/qBittorrent/BT_backup:/BT_backup -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
 ./iyuu.sh
