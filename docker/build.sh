15f990500456abc71f2360893f7285ac514809f3
iyuu.cn <367013672@qq.com>
2020-07-24 13:06:31 +0800

    合并hxsf提交，修改buile.sh

--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,4 @@ rm ./IYUUAutoReseed.zip
 cd /root/IYUUAutoReseed/docker
 chmod +x ./*.sh
 docker build -t iyuu:latest .
-docker run -it -v /root/IYUUAutoReseed:/var/www -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
+docker run -it -v /root/IYUUAutoReseed:/IYUU -p 8510:9000 --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
