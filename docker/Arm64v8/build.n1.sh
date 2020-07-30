a744f5e7854a57bcfeed93e27b6a16ca61514d7e
iyuu.cn <367013672@qq.com>
2020-07-31 07:58:53 +0800

    更改本地ARM平台打包标签为：iyuu:arm64v8

--- docker/Arm64v8/build.n1.sh
+++ docker/Arm64v8/build.n1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -t iyuu:latest .
-docker run -it -v /root/config.php:/config.php -v /var/lib/transmission/torrents:/torrents -v /var/lib/qbittorrent/.local/share/data/qBittorrent/BT_backup:/BT_backup --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
+docker build -t iyuu:arm64v8 .
+docker run -it -v /root/config.php:/config.php -v /var/lib/transmission/torrents:/torrents -v /var/lib/qbittorrent/.local/share/data/qBittorrent/BT_backup:/BT_backup --network bridge --name IYUUAutoReseed --restart always -d iyuu:arm64v8
 docker exec -it IYUUAutoReseed php iyuu.php
--- docker/Arm64v8/build.sh
+++ docker/Arm64v8/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -f Dockerfile -t iyuu:latest .
-docker run -it -v /root/config.php:/config.php --network bridge --name IYUUAutoReseed --restart always -d iyuu:latest
+docker build -f Dockerfile -t iyuu:arm64v8 .
+docker run -it -v /root/config.php:/config.php --network bridge --name IYUUAutoReseed --restart always -d iyuu:arm64v8
 docker exec -it IYUUAutoReseed php iyuu.php
