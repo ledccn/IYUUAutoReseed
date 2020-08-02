fdd5475e373094fb0cb10e6b0c71d64c40dc11c5
iyuu.cn <367013672@qq.com>
2020-08-02 15:38:33 +0800

    兼容qBittorrent v4.1.5【小钢炮等】

--- app/Client/qBittorrent/qBittorrent.php
+++ app/Client/qBittorrent/qBittorrent.php
@@ -236,7 +236,8 @@ class qBittorrent extends AbstractClient
         // Find authentication cookie and set in curl connection
         foreach ($this->curl->response_headers as $header) {
             if (preg_match('/SID=(\S[^;]+)/', $header, $matches)) {
-                $this->curl->setHeader('Cookie', $matches[0]);
+                $qb415 = '; QB_'.$matches[0];   // 兼容qBittorrent v4.1.5[小钢炮等]
+                $this->curl->setHeader('Cookie', $matches[0].$qb415);
                 return true;
             }
         };
